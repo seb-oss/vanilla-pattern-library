c6f471ddb4556dd711f8995d3d3a0f34a0b7da6a
Oskar Mossberg <oskarmo@kth.se>
2019-05-10 13:50:09 +0200

    refactor(textfields): move text field mixins to separate file

--- src/test/test-sass.js
+++ src/test/test-sass.js
@@ -1,7 +1,11 @@
 var path = require('path');
 var sassTrue = require('sass-true');
 
-['inputs/common.spec.scss', 'inputs/stepper.spec.scss']
+[
+  'inputs/common.spec.scss',
+  'inputs/stepper.spec.scss',
+  'inputs/text-field.spec.scss'
+]
   .map(fileName => {
     return path.join(__dirname, fileName);
   })
