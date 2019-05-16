2d4eb91a3be0887ba81b271fe661bf7ae8ae94e4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-16 09:37:13 +0000

    Deploy sebgroup/vanilla-pattern-library to github.com/sebgroup/vanilla-pattern-library.git:gh-pages

---
+++ src/test/test-sass.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+const sassTrue = require('sass-true');
+const _ = require('lodash/array');
+
+const traverseTestsSync = dir => {
+  const list = fs.readdirSync(dir);
+  const files = list.map(file => {
+    const filePath = path.resolve(dir, file);
+    const stats = fs.statSync(filePath);
+    if (stats.isDirectory()) {
+      return traverseTestsSync(filePath);
+    } else {
+      if (path.extname(file) == '.scss') {
+        return filePath;
+      }
+    }
+  });
+  return _.flatten(files.filter(node => node));
+};
+
+traverseTestsSync(__dirname).forEach(file => {
+  sassTrue.runSass({ file }, describe, it);
+});
