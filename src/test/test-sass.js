1d5ccbdda3c6615eddd844261e66519d31811b11
Oskar Mossberg <oskarmo@kth.se>
2019-05-16 11:02:59 +0200

    test(tests): run tests synchronously

--- src/test/test-sass.js
+++ src/test/test-sass.js
@@ -1,48 +1,24 @@
-var path = require('path');
+const path = require('path');
 const fs = require('fs');
-var sassTrue = require('sass-true');
+const sassTrue = require('sass-true');
+const _ = require('lodash/array');
 
-const traverseTests = (dir, done) => {
-  let specs = [];
-
-  fs.readdir(dir, (err, list) => {
-    if (err) return done(err);
-
-    var pending = list.length;
-
-    if (!pending) return done(null, specs);
-
-    list.forEach(file => {
-      const filePath = path.resolve(dir, file);
-
-      fs.stat(filePath, (err, stats) => {
-        if (stats.isDirectory()) {
-          traverseTests(filePath, (err, data) => {
-            specs = specs.concat(data);
-            if (!--pending) done(null, specs);
-          });
-        } else {
-          if (path.extname(file) == '.scss') {
-            specs.push(filePath);
-          }
-          if (!--pending) done(null, specs);
-        }
-      });
-    });
+const traverseTestsSync = dir => {
+  const list = fs.readdirSync(dir);
+  const files = list.map(file => {
+    const filePath = path.resolve(dir, file);
+    const stats = fs.statSync(filePath);
+    if (stats.isDirectory()) {
+      return traverseTestsSync(filePath);
+    } else {
+      if (path.extname(file) == '.scss') {
+        return filePath;
+      }
+    }
   });
+  return _.flatten(files.filter(node => node));
 };
 
-traverseTests(__dirname, (err, data) => {
-  data.forEach(file => {
-    sassTrue.runSass({ file }, describe, it);
-  });
+traverseTestsSync(__dirname).forEach(file => {
+  sassTrue.runSass({ file }, describe, it);
 });
-
-// There is a bug somewhere - had to add this to get the tests running :/
-sassTrue.runSass(
-  {
-    file: `${__dirname}/accordions/accordion.spec.scss`
-  },
-  describe,
-  it
-);
