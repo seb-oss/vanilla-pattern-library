967bba6b141771a4f708a014ee7150a9b0aca057
Oskar Mossberg <oskarmo@kth.se>
2019-06-12 21:36:54 +0200

    chore(applitools): add config and encrypted apikey

---
+++ applitools.config.js
@@ -0,0 +1,7 @@
+const { apiKey } = require('./applitools.key.js');
+
+module.exports = {
+  appName: 'Vanilla Pattern Library',
+  showLogs: true,
+  apiKey
+};
--- cypress/integration/component-button.js
+++ cypress/integration/component-button.js
@@ -1,90 +1,51 @@
 const baseUrl = 'http://localhost:9001';
 const standalonePath = 'osp/standalone/component';
 const suffix = 'preview.html';
+const applitoolsConfig = require('../../applitools.config').default;
 
 const component = {
-  id: 'component-button',
-  variants: [
-    { variant: 'primary', states: 4 },
-    { variant: 'alternative', states: 4 },
-    { variant: 'ghost-light', states: 4 },
-    { variant: 'link', states: 4 },
-    { variant: 'secondary', states: 4 },
-    { variant: 'delete-confirm', states: 4 },
-    { variant: 'delete', states: 4 },
-    { variant: 'ghost-dark', states: 4 }
-  ]
+  id: 'component-button'
+  // variants: [
+  //   { variant: 'primary', states: 4 },
+  //   { variant: 'alternative', states: 4 },
+  //   { variant: 'ghost-light', states: 4 },
+  //   { variant: 'link', states: 4 },
+  //   { variant: 'secondary', states: 4 },
+  //   { variant: 'delete-confirm', states: 4 },
+  //   { variant: 'delete', states: 4 },
+  //   { variant: 'ghost-dark', states: 4 }
+  // ]
 };
 
-//   describe(`Button ${v.variant}`, function() {
-//     const documentationId = `${component.id}-${v.variant}`;
-//     for (let state = 0; state < v.states; state++) {
-//       it(`Visits ${documentationId} in state ${state}`, function() {
-//         cy.visit(
-//           `${baseUrl}/${standalonePath}/${documentationId}/${state}/${suffix}`
-//         );
-
-//         cy.eyesOpen({
-//           appName: 'Vanilla Pattern Library',
-//           testName: `${v.variant} test - ${state}`,
-//           browser: {
-//             deviceName: 'iPhone X',
-//             screenOrientation: 'portrait'
-//           }
-//         });
-
-//         cy.eyesCheckWindow(`${documentationId} - ${state}`);
-
-//         cy.eyesClose();
-//       });
-//     }
-//   });
-// });
-
-describe(`Button primary`, function() {
+describe(`${component.id} - primary`, function() {
   const v = { variant: 'primary', states: 4 };
   const documentationId = `${component.id}-${v.variant}`;
-  it(`Visits ${documentationId}`, function() {
+  before(function() {
     cy.eyesOpen({
-      appName: 'Vanilla Pattern Library',
-      testName: `${v.variant} test`,
-      browser: {
-        deviceName: 'iPhone X',
-        screenOrientation: 'portrait'
-      }
+      ...applitoolsConfig,
+      batchName: documentationId,
+      browser: [
+        { deviceName: 'iPhone X', screenOrientation: 'portrait' },
+        { width: 1024, height: 768, name: 'chrome' },
+        { width: 1440, height: 768, name: 'chrome' },
+        { width: 1440, height: 768, name: 'firefox' },
+        { width: 1440, height: 768, name: 'edge' },
+        { width: 1440, height: 768, name: 'ie11' }
+      ]
     });
+  });
 
+  after(function() {
+    cy.eyesClose();
+  });
+
+  it(`should render all states for ${documentationId}`, () => {
     for (let state = 0; state < v.states; state++) {
       cy.visit(
         `${baseUrl}/${standalonePath}/${documentationId}/${state}/${suffix}`
       );
 
-      cy.eyesCheckWindow({ sizeMode: 'selector', selector: '.sdv-button' });
+      cy.eyesCheckWindow(`${documentationId}, state: ${state}`);
     }
-    cy.eyesClose();
   });
 });
-
-// describe(`Button alternative`, () => {
-//   const v = { variant: 'alternative', states: 4 };
-//   const documentationId = `${component.id}-${v.variant}`;
-//   it(`Visits ${documentationId}`, function() {
-//     cy.eyesOpen({
-//       appName: 'Vanilla Pattern Library',
-//       testName: `${v.variant} test`,
-//       browser: {
-//         deviceName: 'iPhone X',
-//         screenOrientation: 'portrait'
-//       }
-//     });
-
-//     for (let state = 0; state < v.states; state++) {
-//       cy.visit(
-//         `${baseUrl}/${standalonePath}/${documentationId}/${state}/${suffix}`
-//       );
-
-//       cy.eyesCheckWindow(`${documentationId} - ${state}`);
-//     }
-//     cy.eyesClose();
-//   });
-// });
