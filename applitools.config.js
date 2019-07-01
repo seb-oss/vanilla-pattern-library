05d4f81891a53232481166895d5ba38f7273f667
Oskar Mossberg <oskarmo@kth.se>
2019-07-01 15:21:05 +0200

    test: add config file for applitools

--- applitools.config.js
+++ applitools.config.js
@@ -3,5 +3,13 @@ const { apiKey } = require('./applitools.key.js');
 module.exports = {
   appName: 'Vanilla Pattern Library',
   showLogs: true,
-  apiKey
+  apiKey,
+  browser: [
+    { deviceName: 'iPhone X', screenOrientation: 'portrait' },
+    { width: 1024, height: 768, name: 'chrome' },
+    { width: 1440, height: 768, name: 'chrome' },
+    { width: 1440, height: 768, name: 'firefox' },
+    { width: 1440, height: 768, name: 'edge' },
+    { width: 1440, height: 768, name: 'ie11' }
+  ]
 };
